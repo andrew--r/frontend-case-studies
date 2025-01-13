65c6b51a201b04145c93e4eaaac97cbf1d80c144
Andrey Romanov <me@andreyromanov.com>
2025-01-13 00:40:37 +0000

    Update site description

--- website/src/config.ts
+++ website/src/config.ts
@@ -1,4 +1,4 @@
 export const SITE_URL = "https://frontendcs.com";
 export const SITE_NAME = "Frontend Case Studies";
 export const SITE_DESCRIPTION =
-  "A curated list of technical talks and articles about real world enterprise frontend development";
+  "A curated list of talks and articles about real-world frontend development";
